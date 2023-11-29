acf83de7649775f1f3161f0315188702f232d271
ySaudi <106032500+ySaudi@users.noreply.github.com>
2023-11-29 07:48:45 +0300

    Update bitping.sh

--- bitping.sh
+++ bitping.sh
@@ -1,16 +1,7 @@
 #!/bin/bash
 
-# إنشاء المجلد إذا كان غير موجود
 mkdir -p "$HOME/.bitping/"
 
-# تشغيل الحاوية وإجراء الإعدادات اللازمة
 docker run -it --mount type=bind,source="$HOME/.bitping/",target=/root/.bitping bitping/bitping-node:latest
 
-# بعد تشغيل الحاوية وعند المطالبة بالإيميل وكلمة المرور
 echo "Email: beformine@gmail.com"
-read -s -p "Password: " password
-
-# إدخال الإيميل وكلمة المرور داخل الحاوية
-docker exec -it <container_id> sh -c "echo $password > /root/.bitping/password.txt"
-
-# استخدام المتغير $password لإدخال كلمة المرور عند المطلوب
