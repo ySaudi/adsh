2681aa844c4d2b4468e7fd3d169b152da53161f8
ySaudi <106032500+ySaudi@users.noreply.github.com>
2023-12-02 15:10:05 +0300

    Update ca.sh

--- ca.sh
+++ ca.sh
@@ -4,9 +4,6 @@
 apt install docker.io -y
 
 # Pull the Docker image
-docker run -e APPLICATION_KEY=6adfa338-57d0-4112-9476-a1c49ee19828 otohits/app:latest
-
-# Pull the Docker image
 docker pull traffmonetizer/cli_v2
 
 # Run the Docker container with the specified settings
